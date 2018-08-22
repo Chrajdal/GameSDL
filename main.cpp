889353c30ba41ae982b8a538db05202511c0d785
Jan Chráska <jan.chraska@gmail.com>
2018-08-22 20:14:35 +0200

    0.0.0.1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <SDL.h>
+
+#undef main
+
+#pragma comment (lib, "SDL2.lib")
+#pragma comment (lib, "SDL2main.lib")
+#pragma comment (lib, "SDL2_image.lib")
 
 int main(void)
 {
